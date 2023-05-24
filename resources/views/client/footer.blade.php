7a09fd1ecd19ae6a9c749bd9b1bc6c19ba274560
Muhammad_Ardiansyah <86669197+ardiansyahmuhammad@users.noreply.github.com>
2023-05-24 12:15:24 +0700

    Update footer.blade.php

--- resources/views/client/footer.blade.php
+++ resources/views/client/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="bg-light rounded-top p-4">
         <div class="row">
             <div class="col-12 col-sm-6 text-center text-sm-start">
-                &copy; <a href="#">Copyright Kelompok 10 CC | Clinz Laundry
+                &copy; <a href="#">Copyright Kelompok C1 | Clinz Laundry
             </div>
         </div>
     </div>
